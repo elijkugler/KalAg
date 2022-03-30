2a3c8af512eb33368367176b925f3ccf4785a274
elijkugler <eli.j.kugler@outlook.com>
2022-03-30 12:27:44 -0500

    Add project files.

---
+++ KalAg/Views/Home/Farm.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SoilNSunshine.Pages
+{
+    public class FarmModel : PageModel
+    {
+        private readonly ILogger<FarmModel> _logger;
+
+        public FarmModel(ILogger<FarmModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ KalAg/Views/Home/FarmRowCrop.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SoilNSunshine.Pages
+{
+    public class FarmRowCropModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
